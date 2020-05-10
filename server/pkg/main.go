2a75282a5d8fb161cd8addab610fcc602532a06c
Kasia4 <katarzyna.rybacka4@gmail.com>
2020-05-10 20:15:49 +0200

    Update thesis

--- server/pkg/main.go
+++ server/pkg/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
-	"server/pkg/userdata"
 
 	"github.com/gorilla/mux"
 
 	"server/pkg/thesis"
+	"server/pkg/userdata"
 )
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/thesis", thesis.PostTheses).Methods("POST")
+	r.HandleFunc("/thesis", thesis.PutThesis).Methods("PUT")
 	r.HandleFunc("/user-data", userdata.GetAllUserData).Methods("GET")
 	r.HandleFunc("/theses/{id}", userdata.GetUserData).Methods("GET")
 	if err := http.ListenAndServe(":8088", r); err != nil {
