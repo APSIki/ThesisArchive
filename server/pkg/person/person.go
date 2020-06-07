52289a4cb8447e15a0fe6da49707e8186a2c70fa
Kasia4 <katarzyna.rybacka4@gmail.com>
2020-06-07 15:24:54 +0200

    Person endpoint done

--- server/pkg/person/person.go
+++ server/pkg/person/person.go
@@ -2,7 +2,10 @@ package person
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+	"server/pkg/db"
 )
 
 type Person struct {
@@ -10,9 +13,45 @@ type Person struct {
 }
 
 func GetPerson(w http.ResponseWriter, r *http.Request) {
-	//TODO handle {id}
 	w.Header().Set("Content-Type", "application/json")
-	//Mock data - @todo - implement connection with DB
-	personData := []Person{{Name: "Krzysztof Przewodniczącki"}}
-	json.NewEncoder(w).Encode(personData)
+	params := mux.Vars(r) //get parameters
+	var person Person
+	dbConnection := db.GetDB()
+	var query string
+	switch params["id"] {
+	case "1":
+		query = "select first_name, surname from students"
+	case "2":
+		query = "select first_name, surname from staff_person where surname = 'Promotorska'"
+	case "3":
+		query = "select first_name, surname from staff_person where surname = 'Przewodniczącki'"
+	case "4":
+		query = "select first_name, surname from staff_person where surname = 'Członkowska'"
+	case "5":
+		query = "select first_name, surname from staff_person where surname = 'Admiński'"
+	case "6":
+		query = "select first_name, surname from staff_person where surname = 'Przykładowa'"
+	case "7":
+		query = "select first_name, surname from staff_person where surname = 'Elektroniczny'"
+	}
+	rows, err := dbConnection.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var firstName string
+	var surname string
+	for rows.Next() {
+		err := rows.Scan(&firstName, &surname)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if rows.Err() != nil {
+		log.Fatal(err)
+	}
+	name := firstName + " " + surname
+	person = Person{Name: name}
+
+	json.NewEncoder(w).Encode(person)
 }
