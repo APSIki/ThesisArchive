beba4a5207c805cf214404e8bfa85216be5f18ed
Pawel Kowalik (PLK) <PLK@kmd.dk>
2020-06-10 16:15:28 +0200

    added new fiels

--- server/pkg/searchTheses/searchTheses.go
+++ server/pkg/searchTheses/searchTheses.go
@@ -4,9 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"net/url"
 	"server/pkg/db"
-
-	"github.com/gorilla/mux"
 )
 
 type ThesesData struct {
@@ -18,22 +17,38 @@ type ThesesData struct {
 
 func Search(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	kind := params["type"]
-	var query string
-	switch kind {
-	case "all":
-		query = "select thesis.thesis_id, thesis.title, students.first_name, students.surname, thesis_type.name from thesis, thesis_type, students where thesis.thesis_type_id = thesis_type.thesis_type_id and thesis.title is not null and students.session_token = thesis.author_id"
-	case "doctoral":
-		query = "select thesis.thesis_id, thesis.title, students.first_name, students.surname, thesis_type.name from thesis, thesis_type, students where thesis.thesis_type_id = thesis_type.thesis_type_id and thesis.title is not null and students.session_token = thesis.author_id and thesis_type.name = 'Doktorska'"
-	case "master":
-		query = "select thesis.thesis_id, thesis.title, students.first_name, students.surname, thesis_type.name from thesis, thesis_type, students where thesis.thesis_type_id = thesis_type.thesis_type_id and thesis.title is not null and students.session_token = thesis.author_id and thesis_type.name = 'Magisterska'"
-	case "engineering":
-		query = "select thesis.thesis_id, thesis.title, students.first_name, students.surname, thesis_type.name from thesis, thesis_type, students where thesis.thesis_type_id = thesis_type.thesis_type_id and thesis.title is not null and students.session_token = thesis.author_id and thesis_type.name = 'Inżynierska'"
+	
+	u, _ := url.Parse(r.URL.String())
+	v := u.Query()
+
+	query := v["query"]
+	kind := v["type"]
+	author := v["author"]
+	keyword := v["keyword"]
+	defenseDateFrom := v["defenseDateFrom"]
+	defenseDateTo := v["defenseDateTo"]
+	publicationDateFrom := v["publicationDateFrom"]
+	publicationDateTo := v["publicationDateTo"]
+		
+	var sql_query string
+	
+	if len(query) > 0 { 
+		sql_query = `select thesis.thesis_id, thesis.title, students.first_name, students.surname, thesis_type.name from thesis, thesis_type, students 
+		where thesis.title LIKE '%$1%' OR students.first_name LIKE '%$1%' OR students.surname LIKE '%$1%'`;
+	} else {
+		sql_query = `select thesis.thesis_id, thesis.title, students.first_name, students.surname, thesis_type.name from thesis, thesis_type, students, commitee, commitee_person where 
+		(kind IS NULL OR thesis_type.name = $2) AND
+		(author IS NULL OR students.surname LIKE '%$3%') AND
+		(keyword IS NULL OR thesis.key_words LIKE '%$4%') AND
+		(defenseDateFrom IS NULL OR thesis.date > $5) AND
+		(defenseDateTo IS NULL OR thesis.date > $6) AND
+		(publicationDateFrom IS NULL OR thesis.date > $7) AND
+		(publicationDateTo IS NULL OR thesis.date > $8)`
 	}
+
 	thesesData := make([]ThesesData, 0)
 	dbConnection := db.GetDB()
-	rows, err := dbConnection.Query(query)
+	rows, err := dbConnection.Query(sql_query, query, kind, author, keyword, defenseDateFrom, defenseDateTo, publicationDateFrom, publicationDateTo)
 	if err != nil {
 		log.Print(err)
 	}
